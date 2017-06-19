099d99155c070af69dced1d3bcf4f2422be84aac
vinayksk <vinayksk@gmail.com>
2017-06-18 21:45:17 -0700

    fixed the scroll height

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(window).scroll(function(){
     var pos = $(this).scrollTop();
 	var toolbar = document.getElementById("navb").clientHeight;
 	var height2 = window.innerHeight * 2  - toolbar;
-	var height3 = window.innerHeight * 3 - 80;
+	var height3 = window.innerHeight * 3 + 7;
 	var height4 = window.innerHeight * 4 - 120;
 	if(pos < (window.innerHeight - toolbar)) {
         document.getElementById("header").innerHTML = "Welcome";
@@ -15,7 +15,7 @@ $(window).scroll(function(){
         document.getElementById("header").innerHTML = "Experience";
     }
     if(pos > (height3)) {
-        document.getElementById("header").innerHTML = "New text!";
+        document.getElementById("header").innerHTML = "Projects";
     }
     if(pos > (height4)) {
         document.getElementById("header").innerHTML = "New text!";
