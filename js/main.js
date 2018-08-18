364a77309e24f25342e9a4c3d4768b0b8c5a4444
Vinay <vinayksk@gmail.com>
2018-08-17 23:56:14 -0700

    rebuild complete
    massive overhaul of all bootstrap components, everything is now responsive, test files have been converted to index, testing will continue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
-$(document).ready(function(){
-    console.log($(window).height());
+$(document).ready(function () {
     nav = $('.navbar').height();
     final = $(window).height() - nav;
-    console.log(final);
     $('.int').height(final);
-    console.log($('.int').height());
-   })
+})
+
+function smoothScroll(destination) {
+    nav = $('.navbar').height();
+    header = $('.header').height();
+    about = $('#about').height();
+    experience = $('#experience').height();
+    projects = $('#projects').height();
+    var d = 0
+    if (destination == "about"){
+        console.log(destination)
+        d = nav + header + 16;
+        console.log(scroll)
+    }
+    if (destination == "experience"){
+        console.log(destination)
+        d = nav + header + about + 16;
+        console.log(scroll)
+    }
+    if (destination == "projects"){
+        console.log(destination)
+        d = nav + header + about + experience + 16;
+        console.log(scroll)
+    }
+    if (destination == "accolades"){
+        console.log(destination)
+        d = nav + header + about + experience + projects + 16;
+        console.log(scroll)
+    }
+    window.scroll({
+        top: d, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+    
+}
+
+function test(destination){
+    document.querySelector(destination).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
